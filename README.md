3549dbdd34ab805f08eac2e6bdab225f888a7334
mukatirohit <rohanmukati2002@gmail.com>
2024-12-02 15:18:52 +0530

    folder structure updated

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # MLOPS-END-to-END-project-setup
+
+## All Pipeline for mlops
+
+MLOps -> ML + OPS
+1. Data
+2. EDA
+3. Feruee engineering
+4. Model building
+5. Model evaluation
+6. Model deployment
+7. Model monitoring / maintenance
+8. Model retraining
---
+++ template.py
@@ -0,0 +1,47 @@
+import os 
+from pathlib import Path
+import logging
+
+list_of_files = [
+    ".github/workflows.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__py",
+    "src/pipeline/traning_pipeline.py",
+    "src/pipeline/predictions_pipeline.py",
+    "src/utils/__init__.py",
+    "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exception/exception"
+    "test/units/__init__.py",
+    "test/integration/__init__.py",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiments.ipynb"
+    
+    
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename} ")
+        
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+
+
